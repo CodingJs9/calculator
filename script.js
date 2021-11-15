e54d32b1ccb83f07c7835a7375cc01b75faf56f9
Rithvik <rithvikr98@gmail.com>
2021-11-15 17:29:09 +0530

    Made the calculator work

--- script.js
+++ script.js
@@ -2,22 +2,33 @@ const topDisplay = document.querySelector('.top-display')
 const bottomDisplay = document.querySelector('.bottom-display')
 const buttonsGrid = document.querySelector('.buttons')
 
-let topDisplayContent
-let bottomDisplayContent
-
 buttonsGrid.addEventListener('click', handleClick)
+
+let operator
+let firstNumber
+let secondNumber
 
 function handleClick(e) {
 	if (e.target.classList.contains('btn')) {
-		if (
-			e.target.classList.contains('number') ||
-			e.target.classList.contains('operator')
-		) {
+		if (e.target.classList.contains('number')) {
 			bottomDisplay.innerText += e.target.value
-			bottomDisplayContent = bottomDisplay.innerText
+		} else if (e.target.classList.contains('operator')) {
+			firstNumber = parseInt(bottomDisplay.innerText)
+			bottomDisplay.innerText += e.target.value
+			topDisplay.innerText = bottomDisplay.innerText
+			bottomDisplay.innerText = ''
+			operator = e.target.value
+		} else if (e.target.classList.contains('equals')) {
+			secondNumber = parseInt(bottomDisplay.innerText)
+			topDisplay.innerText += bottomDisplay.innerText + '='
+			bottomDisplay.innerText = operate(operator, firstNumber, secondNumber)
+			console.log(secondNumber)
 		} else if (e.target.classList.contains('ac')) {
 			topDisplay.innerText = ''
 			bottomDisplay.innerText = ''
+			firstNumber = 0
+			secondNumber = 0
+			operator = null
 		} else if (e.target.classList.contains('del')) {
 			bottomDisplay.innerText = bottomDisplay.innerText.slice(0, -1)
 		}
@@ -30,6 +41,7 @@ const multiply = (x, y) => x * y
 const divide = (x, y) => x / y
 
 const operate = (operator, x, y) => {
+	if (y === null) return x
 	switch (operator) {
 		case '+':
 			return add(x, y)
