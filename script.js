06f9983065ca7cc7359b55e212fef6adf70a42ac
Rithvik <rithvikr98@gmail.com>
2021-12-05 18:30:02 +0530

    Added semicolons and changed small things

--- script.js
+++ script.js
@@ -1,90 +1,90 @@
-const topDisplay = document.querySelector('.top-display')
-const bottomDisplay = document.querySelector('.bottom-display')
-const buttonsGrid = document.querySelector('.buttons')
+const topDisplay = document.querySelector('.top-display');
+const bottomDisplay = document.querySelector('.bottom-display');
+const buttonsGrid = document.querySelector('.buttons');
 
-buttonsGrid.addEventListener('click', handleClick)
+buttonsGrid.addEventListener('click', handleClick);
 
-let operator
-let firstNumber
-let secondNumber
-let isNegative = false
+let operator;
+let firstNumber;
+let secondNumber;
+let isNegative = false;
 
 function handleClick(e) {
-	if (!e.target.classList.contains('btn')) return
+	if (!e.target.classList.contains('btn')) return;
 	if (e.target.classList.contains('number')) {
-		numberKey(e.target)
+		numberKey(e.target);
 	} else if (e.target.classList.contains('operator')) {
-		operatorKey(e.target)
+		operatorKey(e.target);
 	} else if (e.target.classList.contains('negative')) {
-		changeSign(isNegative)
+		changeSign(isNegative);
 	} else if (e.target.classList.contains('equals')) {
-		equalsKey()
+		equalsKey();
 	} else if (e.target.classList.contains('ac')) {
-		allClear()
+		allClear();
 	} else if (e.target.classList.contains('del')) {
-		backSpace()
+		backSpace();
 	}
 }
 
 const numberKey = (target) => {
-	bottomDisplay.innerText += target.value
-}
+	bottomDisplay.innerText += target.value;
+};
 
 const operatorKey = (target) => {
-	firstNumber = parseFloat(bottomDisplay.innerText) || 0
-	bottomDisplay.innerText += target.value
-	topDisplay.innerText = bottomDisplay.innerText
-	bottomDisplay.innerText = ''
-	operator = target.value
-}
+	firstNumber = parseFloat(bottomDisplay.innerText) || 0;
+	bottomDisplay.innerText += target.value;
+	topDisplay.innerText = bottomDisplay.innerText;
+	bottomDisplay.innerText = '';
+	operator = target.value;
+};
 
 const changeSign = (isNegative) => {
-	isNegative = !isNegative
-	if (isNegative === true) {
+	isNegative = !isNegative;
+	if (isNegative) {
 		bottomDisplay.innerText =
 			parseFloat(bottomDisplay.innerText) -
-			parseFloat(bottomDisplay.innerText * 2)
+			parseFloat(bottomDisplay.innerText * 2);
 	}
-}
+};
 
 const equalsKey = () => {
-	secondNumber = parseFloat(bottomDisplay.innerText) || 0
-	topDisplay.innerText += bottomDisplay.innerText + '='
-	bottomDisplay.innerText = round(operate(operator, firstNumber, secondNumber))
-}
+	secondNumber = parseFloat(bottomDisplay.innerText) || 0;
+	topDisplay.innerText += `${bottomDisplay.innerText}=`;
+	bottomDisplay.innerText = round(operate(operator, firstNumber, secondNumber));
+};
 
 const allClear = () => {
-	topDisplay.innerText = ''
-	bottomDisplay.innerText = ''
-	firstNumber = 0
-	secondNumber = 0
-	operator = null
-}
+	topDisplay.innerText = '';
+	bottomDisplay.innerText = '';
+	firstNumber = 0;
+	secondNumber = 0;
+	operator = null;
+};
 
 const backSpace = () => {
-	bottomDisplay.innerText = bottomDisplay.innerText.slice(0, -1)
-}
+	bottomDisplay.innerText = bottomDisplay.innerText.slice(0, -1);
+};
 
-const round = (x) => Math.round(x * 1000) / 1000
+const round = (x) => Math.round(x * 1000) / 1000;
 
-const add = (x, y) => x + y
-const subtract = (x, y) => x - y
-const multiply = (x, y) => x * y
-const divide = (x, y) => x / y
+const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
 
 function operate(operator, x, y) {
 	switch (operator) {
 		case '+':
-			return add(x, y)
-			break
+			return add(x, y);
+			break;
 		case '-':
-			return subtract(x, y)
-			break
+			return subtract(x, y);
+			break;
 		case '×':
-			return multiply(x, y)
-			break
+			return multiply(x, y);
+			break;
 		case '÷':
-			return divide(x, y)
-			break
+			return divide(x, y);
+			break;
 	}
 }
